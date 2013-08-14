feb34403fd2cc628f84ee77199a69268c686611a
techird <techirdliu@gmail.com>
2013-08-14 14:19:49 +0800

    url fix

--- fex/case/cases/background/conf.php
+++ fex/case/cases/background/conf.php
@@ -3,7 +3,7 @@ $conf = array(
     "demos" => array("background"),
     "qrImages" => array(
         array(
-            "link" => "http://xiangce.baidu.com/pad",
+            "link" => "http://xiangce.baidu.com/pad/",
             "text" => "相册 (iPad Only)"
         )
     )
